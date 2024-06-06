51e40f51eff210efd5d57e283aa78e7f1fc62a35
0xjosiah <josiah.webb@siliconmtn.com>
2024-06-06 17:12:34 -0600

    set up

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import ForceGraph from 'react-force-graph-2d';
+
+const graphData = {
+  nodes: [
+    { id: 'a' },
+    { id: 'b' },
+    { id: 'c' },
+    { id: 'd' },
+  ],
+  links: [
+    { source: 'a', target: 'b' },
+    { source: 'c', target: 'a' },
+    { source: 'd', target: 'a' },
+  ]
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ForceGraph graphData={graphData} />
   );
 }
 
